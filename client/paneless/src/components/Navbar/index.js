3439f7c7cc84e20eae991a720426bd239f149d30
Jonathon <>
2023-01-18 13:58:03 -0700

    changed the style of the navbar to be more reactive and stikcy

--- client/paneless/src/components/Navbar/index.js
+++ client/paneless/src/components/Navbar/index.js
@@ -4,76 +4,73 @@ import logo from '../../assets/images/transparent.png'
 import { faHome, faInfoCircle, faUser, faUserPlus, faFire,} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState,  } from 'react';
+import { Component } from "react";
 
 
 
-const Navbarr = () => {
-
-
- const [navbar,setNavbar] = useState(false);
-
-
-
-
-
-
- 
- const changeBackground = () => {
-  if(window.scrollY >= 80) {
-    setNavbar(true)
-  } else {
-    setNavbar(false);
+class  Navbarr extends Component {
+  state = {clicked: false };
+  handleClick = () =>{
+    this.setState({clicked: !this.state.clicked})
   }
-  console.log(window.scrollY)
- }
-window.addEventListener('scroll', changeBackground);
-
-  return (
-    <>
-  <nav className={navbar ? 'navbar active' : 'navbar'}>
-   <div className="navbar-container">
-   <img src={logo} alt="placholder" className="logo" />
-            <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faHome} color="#000000"/>&nbsp;
-                <b>Home</b> &nbsp;|
-            </NavLink>
-            <NavLink exact="true" activeclassname="active"  className="about-link"  to="/about">
-            <FontAwesomeIcon icon={faInfoCircle} color="#000000"/>&nbsp;
-            <b>About Us</b> &nbsp;|
-                
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="featured-link" to="/featured">
-            <FontAwesomeIcon icon={faFire} color="#000000"/>&nbsp;
-            <b>Featured</b> &nbsp;|
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="quote-link" to="/quote">
-           
-               Request Quote&nbsp; |
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="window-link" to="/resume">
-          
-               Window Screens &nbsp;|
-            </NavLink>
-          
-           
-           
-           
-              <NavLink exact="true" activeclassname="active" className="login-link" to="/resume">
-              <FontAwesomeIcon icon={faUser} color="#000000"/>&nbsp;
-              <b>Login</b> |
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="create-link" to="/resume">
-            <FontAwesomeIcon icon={faUserPlus} color="#000000"/>
-              Sign Up for Free
-            </NavLink>
-            
-           
-      </div>
-   </nav>
-
-
   
+  render() {
+return(
+  <>
+  <nav>
+    <img className='logo' src={logo} alt="logo" />;
+
+  <div> 
+    <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
+      <li>
+        <NavLink exact="true" activeclassName="active" to='/'>
+        <FontAwesomeIcon icon={faHome} color="orange"/>
+        Home |
+        </NavLink>
+        
+      </li>
+      <li>
+      <NavLink exact="true" activeclassName="active" to='/About'>
+        <FontAwesomeIcon icon={faInfoCircle} color="orange"/>
+        About Us |
+        </NavLink>
+      </li>
+      <li>
+      <NavLink exact="true" activeclassName="active" to='/About'>
+        <FontAwesomeIcon icon={faFire} color="orange"/>
+        Featured |
+        </NavLink>
+      </li>
+      <li>
+      <NavLink exact="true" activeclassName="active" to='/About'>
+        
+        Window Screens | 
+        </NavLink>
+      </li>
+      <li>
+      <NavLink className="login-link" exact="true" activeclassName="active" to='/About'>
+        <FontAwesomeIcon icon={faUser} color="orange"/>
+        Login 
+        </NavLink>
+      </li>
+      <li>
+      <NavLink className="signup-link"exact="true" activeclassName="active" to='/About'>
+        <FontAwesomeIcon icon={faUserPlus} color="orange"/>
+        Sign Up for Free 
+        </NavLink>
+      </li>
+    </ul>
+  </div>
+  <div id="mobile" onClick={this.handleClick}>
+    <i id="bar"
+    className={this.state.clicked ? 'fas fa-times' :
+  "fas fa-bars"}
+    ></i>
+  </div>
+  </nav>
   </>
-  );
+)
 }
+}
+
 export default Navbarr
