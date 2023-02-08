ab6bd695a61a260dd60c1a5e38fbf7ee5422efe3
Jonathon <>
2023-02-07 21:50:44 -0700

    final changes to contact page

--- client/src/components/Quote/index.js
+++ client/src/components/Quote/index.js
@@ -1,17 +1,62 @@
 import './index.scss';
-import React from 'react';
 import Loader from 'react-loaders';
-import { faUserPlus, } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
 
 
 
 
 
  const Quote = () => {
+
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_og68bwu', 'template_zbjom5y', form.current, 'zEjs_2nVXooZbD7aG')
+        .then((result) => {
+            console.log(result.text);
+            alert("Email Has Been Sent!")
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
  
   return (<>
-   <div className="outline"></div>
+  
+   <div className="outline">
+   <h2 className="contact-us">Contact Us</h2>
+        <form className="email-form"ref={form} onSubmit={sendEmail}>   
+        <i><FontAwesomeIcon className="stary" icon={faEnvelope} size="4x"color="orange"/></i>
+            <div className="layout-form">
+      
+                <input placeholder="First name" type="text"></input>&nbsp;&nbsp;
+                <input placeholder="Last name"  type="text"></input>
+            </div>
+
+            <div className="layout-form">
+                <input placeholder="Phone number" type="text"></input>&nbsp;&nbsp;
+                <input placeholder="Email address"type="text"></input>
+            </div>
+
+            <div>
+                <input className="address-quote" placeholder="Address"></input>
+        
+            </div>
+
+            <div className="layout-form">
+                <input placeholder="City"></input>&nbsp;&nbsp;
+                <input placeholder="Zip"></input>
+            </div>
+           
+            <input className="send"type="submit" value="Send" />
+        </form>
+     
+        
+    
+   </div>
   </>
   )
 }
